b47f11b1b70f791cee3ba64e2dd4b48114d4e615
Chaitanya Singh <159553178+EC-Chaitanya@users.noreply.github.com>
2026-04-24 13:52:20 +0530

    Delete FRONTEND directory

--- FRONTEND/src/app/components/DistractionAlerts/index.ts
+++
@@ -1,5 +0,0 @@
-export { SoftWarning } from "./SoftWarning";
-export { StrongWarning } from "./StrongWarning";
-export { MemeAlert } from "./MemeAlert";
-export { DistractionMonitor, type DistractionMonitorProps, type DistractionState } from "./DistractionMonitor";
-export { DistractionSettings, type DistractionSettings } from "./DistractionSettings";
